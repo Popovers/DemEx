abb5dd62f62059fb51968f10ab185eaf52a4fae8
Popovers <103516371+Popovers@users.noreply.github.com>
2024-05-30 13:13:56 +0300

    Add files via upload

---
+++ demEx/src/module-info.java
@@ -0,0 +1,7 @@
+module demEx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires javafx.graphics;
+    opens application to javafx.graphics, javafx.fxml, javafx.base;
+}
